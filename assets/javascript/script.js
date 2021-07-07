6c9e84bb93007da7fe0ca0a74a9f14bee65f4c2c
merkurio02 <alejandro.esp.silv@gmail.com>
2021-07-06 22:26:13 -0400

    segundo commit
    funcionamiento casi completo

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,9 +1,135 @@
+
+
+
+let userhand;
+let cpuhand;
+let winerhand;
+
+
+
 $(document).ready(function () {
 
-    let piedra = "fa-hand-rock";
-    let papel = "fa-hand-paper";
-    let tijera = "fa-hand-scissors";
+    $('#btn-piedra').click(function () {
+        start(0);
 
+    }
+    );
+    $('#btn-papel').click(function () {
+        start(1);
 
+    }
+    );
+    $('#btn-tijeras').click(function () {
+        start(2);
 
+    }
+    );
 });
+
+function start(hand) {
+    remake();
+    setTimeout(() => {
+        $('.hands').css({ "background-color": "black", "color": "white" });
+        setTimeout(() => {
+            $('.hands').css({ "background-color": "white", "color": "black" });
+            setTimeout(() => {
+                play(hand);
+            }, 500);
+        }, 500);
+
+    }, 500);
+
+}
+
+
+function play(hand) {
+
+
+    cpuhand = getRandomHand();
+
+    userhand = hand;
+    console.log(userhand + " " + cpuhand);
+
+    if (userhand == cpuhand) {
+        $('#mensaje').text("EMPATE");
+        $('#user-border').addClass("border-success");
+        $('#cpu-border').addClass("border-success");
+
+    }
+    else {
+        winerhand = whoWhin(userhand, cpuhand);
+        console.log(winerhand);
+        if (winerhand == userhand) {
+            $('#mensaje').text("GANASTE");
+            $('#user-border').addClass("border-success");
+            $('#cpu-border').addClass("border-danger");
+        }
+        else {
+            $('#mensaje').text("GANA LA MAQUINA");
+            $('#user-border').addClass("border-danger");
+            $('#cpu-border').addClass("border-success");
+        }
+    }
+
+    $('#cpu-hand').removeClass(getHand(-1)).addClass(getHand(cpuhand));
+    $('#user-hand').removeClass(getHand(-1)).addClass(getHand(userhand));
+
+
+
+
+
+
+}
+
+
+function whoWhin(u, c) {
+    if (u == 0)
+        if (c == 2)
+            return 0;
+        else
+            return 1;
+    else
+        if (u == 1)
+            if (c == 0)
+                return 1;
+            else
+                return 2;
+        else
+            if (c == 0)
+                return 0
+            else
+                return 2;
+
+
+
+}
+
+
+function getRandomHand() {
+    return Math.floor(Math.random() * 45) % 3;
+
+}
+
+function getHand(number) {
+
+    switch (number) {
+        case 0:
+            return "fa-hand-rock";
+            break;
+        case 1:
+            return "fa-hand-paper";
+            break;
+        case 2:
+            return "fa-hand-scissors";
+        default:
+            return "fa-question-circle";
+    }
+}
+
+function remake() {
+    $('.hands').removeClass(getHand(cpuhand)).removeClass(getHand(userhand)).addClass(getHand(-1)).removeClass('border-danger').removeClass('border-success') ;
+
+
+
+}
+
